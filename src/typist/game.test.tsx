abf31a6102d18185ad0e4e1801c3cd44a7bcfe20
Jaakko <jaakko.tulkki@gmail.com>
2018-06-17 14:53:07 +0100

    Added gme rules and tests

--- src/typist/game.test.tsx
+++ src/typist/game.test.tsx
@@ -1,66 +1,147 @@
-import {Game, Key, GameTimer} from "./game";
+import {Game, Key, GameTimer, TypedKey, GameResults} from "./game";
 
 describe('Game', () => {
-    function createGame() {
-        const game: Game = new Game(new GameTimer());
-        game.sendKey(new Key('a'));
-        game.sendKey(new Key('a'));
-        game.sendKey(new Key('b'));
-        return game;
+    let game: Game;
+    beforeEach(() => {
+        game = new Game(new GameTimer());
+    });
+
+    function addStrokesToGame(strokes: string) {
+        for(let key of strokes) {
+            game.sendKey(new Key(key));
+        }
     }
 
+    function addTextToGame(text: string) {
+        game.addText(text);
+    }
+
+    it('should throw error if no text and trying to send keystroke', () => {
+        expect(() => game.sendKey(new Key('a'))).toThrow('You do not have added text to type.')
+    });
+
     it('should record keystrokes', () => {
-        const game: Game = createGame();
+        addTextToGame('hello');
+        addStrokesToGame('hab');
 
-        const playedStrokes: Key[] = game.readKeys();
+        const playedStrokes: TypedKey[] = game.getResults().keys();
 
-        expect(playedStrokes).toEqual([new Key('a'), new Key('a'), new Key('b')]);
+        expect(playedStrokes).toEqual([
+            new TypedKey('h', true),
+            new TypedKey('a', false),
+            new TypedKey('b', false),
+        ]);
     });
 
     it('should delete keystrokes', () => {
-        const game: Game = createGame();
+        addTextToGame('haa');
+        addStrokesToGame('baa');
         game.delete();
         game.delete();
 
-        const playedStrokes: Key[] = game.readKeys();
+        const playedStrokes: Key[] = game.getResults().keys();
 
-        expect(playedStrokes).toEqual([new Key('a')]);
+        expect(playedStrokes).toEqual([new TypedKey('b', false)]);
     });
 
-    it('should tell how many keystrokes per minute player is hitting [mock]', () => {
-        const start = jest.fn();
-        const timer = {
-            start,
-            getTime: function () {
-                return this.seconds;
-            },
-            seconds: 3
-        };
-
-        const game: Game = new Game(timer as any);
-        game.sendKey(new Key('a'));
-        game.sendKey(new Key('b'));
-        const keysPerMinute: number = game.keysPerMinute();
-
-        expect(start).toHaveBeenCalled();
-        expect(keysPerMinute).toEqual(40);
-    });
-
-    it('[do not mock] should tell how many keystrokes per minute player is hitting', (done) => {
-        const timer: GameTimer = new GameTimer();
-
-        const game: Game = new Game(timer);
-        game.sendKey(new Key('a'));
-        game.sendKey(new Key('a'));
-        game.sendKey(new Key('b'));
-        game.sendKey(new Key('b'));
+    it('should tell how many keystrokes per minute player is hitting', (done) => {
+        addTextToGame('chicken');
+        addStrokesToGame('chii');
         game.delete();
-        game.sendKey(new Key('b'));
+        addStrokesToGame('c');
+
         setTimeout(() => {
-            const keysPerMinute: number = game.keysPerMinute();
+            const keysPerMinute: number = game.getResults().keysPerMinute();
             expect(keysPerMinute).toEqual(240);
+
             done();
         }, 1001);
     });
+
+    it('should tell how many how many correct and incorrect characters have been typed', (done) => {
+        addTextToGame('hello');
+        addStrokesToGame('heli');
+
+        setTimeout(() => {
+            game.sendKey(new Key('o'));
+            const results: GameResults = game.getResults();
+
+            expect(results.keys()).toEqual([
+                new TypedKey('h', true),
+                new TypedKey('e', true),
+                new TypedKey('l', true),
+                new TypedKey('i', false),
+                new TypedKey('o', true),
+            ]);
+            expect(results.keysPerMinute()).toEqual(240);
+
+            done();
+        }, 1001);
+    });
+
+    it('should end the game once everything has been typed correctly', () => {
+        addTextToGame('hello there');
+        addStrokesToGame('hellos');
+        game.delete();
+        addStrokesToGame(' there');
+
+        expect(game.isFinished()).toBeTruthy();
+    });
+
+    it('should not stop the game if it is not yet over', () => {
+        addTextToGame('hello');
+        addStrokesToGame('hell');
+
+        expect(game.isFinished()).toEqual(false);
+    });
+
+    it('should stop the timer once the game has stopped', (done) => {
+        addTextToGame('hello');
+        addStrokesToGame('h');
+
+        setTimeout(() => {
+            addStrokesToGame('ello');
+        }, 1001);
+
+        setTimeout(() => {
+            expect(game.isFinished()).toBeTruthy();
+            expect(game.getResults().seconds).toEqual(1);
+            done();
+        }, 2001);
+
+    });
+
+    it('should throw an error if trying to keep playing once game ended', () => {
+        addTextToGame('hello');
+        addStrokesToGame('hello');
+
+        expect(() => addStrokesToGame('w')).toThrow('Game has ended.')
+    });
+
+    it('should not delete if there is nothing to delete', () => {
+        addTextToGame('aa');
+        addStrokesToGame('a');
+        game.delete();
+        game.delete();
+
+        expect(game.getResults().keys()).toEqual([]);
+    });
 });
 
+describe('GameResults', () => {
+    it('should tell correct keys per minute', () => {
+        const text = 'This is a long Text to Write';
+        const keyStrokes = text.split('').map((key) => {
+            // Upper case letters are spelled incorrect in this test
+            let correct = !key.startsWith(key.toUpperCase()) || key === ' ';
+            return new TypedKey(key, correct);
+        });
+
+        const seconds = 10;
+        const expectedKeysPerMinute = (text.length - 3) / (seconds / 60) ;
+
+        const results = new GameResults(keyStrokes, seconds);
+
+        expect(results.keysPerMinute()).toEqual(expectedKeysPerMinute);
+    });
+});
--- src/typist/game.tsx
+++ src/typist/game.tsx
@@ -1,23 +1,73 @@
 export class Game {
-    private keys: Key[] = [];
-    constructor(private timer: GameTimer){
-        timer.start();
-    }
+    private keys: TypedKey[] = [];
+    private textToType: string;
+    private currentPointer: number = 0;
+    private gameInPlay: boolean = true;
+
+    constructor(private timer: GameTimer){ }
 
     public sendKey(key: Key): void {
-        this.keys.push(key);
-    }
+        if(!this.textToType) {
+            throw new Error('You do not have added text to type.');
+        }
+        if(!this.gameInPlay) {
+            throw new Error('Game has ended.');
+        }
 
-    public readKeys(): Key[] {
-        return this.keys;
+        if(!this.timer.gameHasStarted()) {
+            this.timer.start();
+        }
+
+        let correct = true;
+        if(this.textToType[this.currentPointer] !== key.key) {
+            correct = false;
+        }
+        this.currentPointer += 1;
+
+        this.keys.push(new TypedKey(key.key, correct));
+
+        if(this.shouldEndGame()) {
+            this.endGame();
+        }
     }
 
     public delete(): void {
-        this.keys.pop();
+        if(this.currentPointer > 0) {
+            this.currentPointer -= 1;
+            this.keys.pop();
+        }
+    }
+
+    addText(textToType: string) {
+        this.textToType = textToType;
+    }
+
+    getResults(): GameResults {
+        return new GameResults(this.keys, this.timer.getTime());
+    }
+
+    isFinished(): boolean {
+        return this.timer.gameHasStarted() && this.currentPointer === this.textToType.length;
+    }
+
+    private shouldEndGame() {
+        return this.currentPointer === this.textToType.length;
+    }
+
+    private endGame() {
+        this.gameInPlay = false;
+        this.timer.stop();
+    }
+}
+
+export class GameResults {
+    constructor(private typedKeys: TypedKey[], public seconds: number) {}
+    public keys(): TypedKey[] {
+        return this.typedKeys;
     }
 
     public keysPerMinute(): number {
-        return this.keys.length / (this.timer.getTime() / 60)
+        return this.typedKeys.filter(key => key.correct).length / (this.seconds / 60)
     }
 }
 
@@ -41,7 +91,15 @@ export class GameTimer {
         return this.seconds;
     }
 
+    public gameHasStarted() {
+        return !!this.interValId;
+    }
+
     public stop() {
         clearInterval(this.interValId);
     }
+}
+
+export class TypedKey {
+    constructor(public key: string, public correct: boolean) {}
 }
